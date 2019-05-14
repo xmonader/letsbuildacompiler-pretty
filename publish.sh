26416aa9685aaa8cd55620cf0f4410fa75cf1f38
xmonader <xmonader@gmail.com>
2019-05-14 19:45:08 +0200

    add epub, html, pdf output

--- publish.sh
+++ publish.sh
@@ -1,3 +1,10 @@
 set -ex
 mdbook build -d docs
 cp -f highlight.js docs/
+
+crowbook letsbuildacompiler.book 
+
+mkdir -p release
+mv letsbuildacompiler.pdf release/
+mv letsbuildacompiler.epub release/
+mv letsbuildacompiler.html release/
